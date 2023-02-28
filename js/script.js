7dbcac6768992a887e271cf6b37e68091da57130
VOOTRIC <victorspacheco2004@gmail.com>
2023-02-28 15:03:24 -0300

    Menu ativo adicionado ao site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('teste');
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+
+    if (url.includes(href)) {
+        link.classList.add('ativo');
+    }
+
+}
+
+links.forEach(ativarLink);
