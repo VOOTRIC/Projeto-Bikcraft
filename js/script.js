ad92f82bc3375895c0da8d73de2470820d8b6ef3
VOOTRIC <victorspacheco2004@gmail.com>
2023-03-02 04:57:29 -0300

    Parametros de URL adicionados

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Ativar links do menu 
+
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -12,3 +14,20 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orcamento
+
+const parametros = new URLSearchParams(location.search);
+
+
+function ativarProduto(parametro) {
+
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
+
