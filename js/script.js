5a628c5a9c2e104233c65f77804cb1dbc0113b5c
VOOTRIC <victorspacheco2004@gmail.com>
2023-03-02 11:00:50 -0300

    Galeria com foco de imagem usando JS  adicionada

--- js/script.js
+++ js/script.js
@@ -31,3 +31,48 @@ function ativarProduto(parametro) {
 
 parametros.forEach(ativarProduto);
 
+
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+
+// Mudar a Imagem em destaque 
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+
+function trocarImagem(evento) {
+    const img = evento.currentTarget;
+    const media = matchMedia('(min-width: 1000px)').matches;
+
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+
+
+galeria.forEach(eventosGaleria);
